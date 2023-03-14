c3699318f8200bc3834a82d581ff7c9a571baf4c
Stan Goldin <salty87@gmail.com>
2023-03-14 09:42:31 +0200

    fix test

--- TestGifSineMaker/TestGifLoaderAndMaker.cs
+++ TestGifSineMaker/TestGifLoaderAndMaker.cs
@@ -1,83 +1,83 @@
 ﻿using GifMotion;
 using System.Drawing;
 
-namespace GifsineMaker.Tests
+namespace GifsineMaker.Tests;
+
+public class GifLoaderTests : IDisposable
 {
-    public class GifLoaderTests
+    private readonly string _testInputFile;
+    private readonly string _testOutputFile;
+
+    public GifLoaderTests()
     {
-        private static byte[] TestData => new byte[] { 71, 73, 70, 56, 57, 97, 2, 0, 2, 0, 128, 0, 0, 255, 255, 255, 0, 0, 0, 33, 249, 4, 9, 5, 0, 2, 0, 44, 0, 0, 0, 0, 2, 0, 2, 0, 0, 2, 7, 140, 69, 0, 59 };
-
-        [Fact]
-        public void Constructor_LoadsGifIntoFramesList()
+        _testInputFile = Path.Combine(Path.GetTempPath(), $"{Guid.NewGuid()}.gif");
+        _testOutputFile = Path.Combine(Path.GetTempPath(), $"{Guid.NewGuid()}.gif");
+        using (var gifImage = new Bitmap(100, 100))
         {
-            // Arrange
-            string inputFile = "test.gif";
-            File.WriteAllBytes(inputFile, TestData);
-
-            // Act
-            GifLoader gifLoader = new(inputFile);
-
-            // Assert
-            Assert.NotNull(gifLoader);
-            Assert.Single(gifLoader.Frames);
+            using var creator = AnimatedGif.Create(_testInputFile, 100);
+            creator.AddFrame(gifImage, 100);
+            creator.AddFrame(gifImage, 100);
+            creator.AddFrame(gifImage, 100);
         }
+    }
 
-        [Fact]
-        public void Constructor_AddReverse()
-        {
-            // Arrange
-            string inputFile = "test.gif";
-            File.WriteAllBytes(inputFile, TestData);
+    [Fact]
+    public void Constructor_LoadsGifIntoFramesList()
+    {
+        // Act
+        GifLoader gifLoader = new(_testInputFile);
 
-            // Act
-            GifLoader gifLoader = new(inputFile);
-            gifLoader.AddReverse(false);
+        // Assert
+        Assert.NotNull(gifLoader);
+        Assert.Equal(3, gifLoader.Count);
+    }
 
-            // Assert
-            Assert.NotNull(gifLoader);
-            Assert.Equal(2, gifLoader.Frames.Count);
-        }
+    [Fact]
+    public void Constructor_AddReverse()
+    {
+        // Act
+        GifLoader gifLoader = new(_testInputFile);
+        gifLoader.AddReverse(false);
 
-        [Fact]
-        public void Constructor_ThrowsFileNotFoundException_WhenInputFileDoesNotExist()
-        {
-            //Arrange
-            string inputFile = "nonexistent.gif";
+        // Assert
+        Assert.NotNull(gifLoader);
+        Assert.Equal(6, gifLoader.Frames.Count);
+    }
 
-            //Act and assert
-            Assert.Throws<FileNotFoundException>(() => new GifLoader(inputFile));
-        }
+    [Fact]
+    public void Constructor_ThrowsFileNotFoundException_WhenInputFileDoesNotExist()
+    {
+        // Arrange
+        string inputFile = "nonexistent.gif";
 
-        [Fact]
-        public void TestGifMaker()
-        {
-            // Create a test GIF file with 3 frames, each with a delay of 100 ms
-            string inputFile = "program_test_input.gif";
-            using (Image gifImage = new Bitmap(100, 100))
-            using (GifCreator creator = AnimatedGif.Create(inputFile, 100))
-            {
-                creator.AddFrame(gifImage, 100);
-                creator.AddFrame(gifImage, 100);
-                creator.AddFrame(gifImage, 100);
-            }
+        // Act and assert
+        Assert.Throws<FileNotFoundException>(() => new GifLoader(inputFile));
+    }
 
-            // Create a GifMaker object with the test input file
-            GifMaker maker = new(inputFile,false);
+    [Fact]
+    public void TestGifMaker()
+    {
+        // Create a GifMaker object with the test input file
+        GifMaker maker = new(_testInputFile, false);
 
-            // Modify the delays using a sine wave with maxDelayRatio = 1 and speedUpRatio = 1
-            maker.NewDelays(1, 1);
+        // Modify the delays using a sine wave with maxDelayRatio = 1 and speedUpRatio = 1
+        maker.NewDelays(1, 1);
 
-            // Save the modified GIF to an output file
-            string outputFile = "program_test_output.gif";
-            maker.SaveGif(outputFile);
+        // Save the modified GIF to an output file
+        maker.SaveGif(_testOutputFile);
 
-            // Load the output file using the GifLoader class
-            GifLoader loader = new(outputFile);
+        // Load the output file using the GifLoader class
+        GifLoader loader = new(_testOutputFile);
 
-            // Verify that the output file has the expected properties
-            Assert.Equal(6, loader.Count); // 3 original frames + 3 reversed frames
-            Assert.Equal(100, loader.Frames[0].Delay); // first frame should have the same delay as before
-            Assert.True(loader.Frames.Skip(1).Select(f => f.Delay).All(d => d >= 0)); // all other delays should be non-negative
-        }
+        // Verify that the output file has the expected properties
+        Assert.Equal(6, loader.Count); // 3 original frames + 3 reversed frames
+        Assert.Equal(100, loader.Frames[0].Delay); // first frame should have the same delay as before
+        Assert.True(loader.Frames.Skip(1).Select(f => f.Delay).All(d => d >= 0)); // all other delays should be non-negative
+    }
+
+    public void Dispose()
+    {
+        File.Delete(_testInputFile);
+        File.Delete(_testOutputFile);
     }
 }
--- TestGifSineMaker/testProgram.cs
+++ TestGifSineMaker/testProgram.cs
@@ -1,50 +1,75 @@
-﻿namespace SineWaveGIF.Tests;
+﻿using GifMotion;
 
-public class ProgramTests
+namespace SineWaveGIF.Tests
 {
-    private const string TestInputFile = "test_input.gif";
-    private const string TestOutputFile = "test_output.gif";
-
-    [Fact]
-    public void Main_ValidArguments_ShouldCreateSineWaveGIF()
+    public class ProgramTests : IDisposable
     {
-        // Arrange
-        string[] args = new[] { TestInputFile, TestOutputFile, "1", "1" };
+        private readonly string _testInputFile;
+        private readonly string _testOutputFile;
 
-        // Act
-        Program.Main(args);
+        public ProgramTests()
+        {
+            _testInputFile = Path.Combine(Path.GetTempPath(), $"{Guid.NewGuid()}.gif");
+            _testOutputFile = Path.Combine(Path.GetTempPath(), $"{Guid.NewGuid()}.gif");
 
-        // Assert
-        Assert.True(File.Exists(TestOutputFile));
-    }
+            using (var gifImage = new System.Drawing.Bitmap(100, 100))
+            {
+                using (var gifCreator = AnimatedGif.Create(_testInputFile, 100, -1))
+                {
+                    gifCreator.AddFrame(gifImage, delay: 100);
+                    gifCreator.AddFrame(gifImage, delay: 100);
+                    gifCreator.AddFrame(gifImage, delay: 100);
+                }
+            }
+        }
 
-    [Fact]
-    public void Main_InvalidArguments_ShouldThrowArgumentException()
-    {
-        // Arrange
-        string[] args = new[] { "arg1", "arg2", "arg3" };
+        [Fact]
+        public void Main_ValidArguments_ShouldCreateSineWaveGIF()
+        {
+            // Arrange
+            string[] args = new[] { _testInputFile, _testOutputFile, "1", "1" };
 
-        // Act and Assert
-        Assert.Throws<ArgumentException>(() => Program.Main(args));
-    }
+            // Act
+            Program.Main(args);
 
-    [Fact]
-    public void Main_InvalidMaxDelayRatio_ShouldThrowArgumentException()
-    {
-        // Arrange
-        string[] args = new[] { TestInputFile, TestOutputFile, "invalid", "0.5" };
+            // Assert
+            Assert.True(File.Exists(_testOutputFile));
+        }
 
-        // Act and Assert
-        Assert.Throws<ArgumentException>(() => Program.Main(args));
-    }
+        [Fact]
+        public void Main_InvalidArguments_ShouldThrowArgumentException()
+        {
+            // Arrange
+            string[] args = new[] { "arg1", "arg2", "arg3" };
 
-    [Fact]
-    public void Main_InputFileNotFound_ShouldThrowArgumentException()
-    {
-        // Arrange
-        string[] args = new[] { "invalid_file_path", TestOutputFile, "1", "1" };
+            // Act and Assert
+            Assert.Throws<ArgumentException>(() => Program.Main(args));
+        }
 
-        // Act and Assert
-        Assert.Throws<ArgumentException>(() => Program.Main(args));
+        [Fact]
+        public void Main_InvalidMaxDelayRatio_ShouldThrowArgumentException()
+        {
+            // Arrange
+            string[] args = new[] { _testInputFile, _testOutputFile, "invalid", "0.5" };
+
+            // Act and Assert
+            Assert.Throws<ArgumentException>(() => Program.Main(args));
+        }
+
+        [Fact]
+        public void Main_InputFileNotFound_ShouldThrowArgumentException()
+        {
+            // Arrange
+            string[] args = new[] { "invalid_file_path", _testOutputFile, "1", "1" };
+
+            // Act and Assert
+            Assert.Throws<ArgumentException>(() => Program.Main(args));
+        }
+
+        public void Dispose()
+        {
+            File.Delete(_testInputFile);
+            File.Delete(_testOutputFile);
+        }
     }
 }
