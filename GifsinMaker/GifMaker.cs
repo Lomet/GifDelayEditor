f093cb89021e6326d6fbcc59f67ea2623e4f1754
Stan Goldin <salty87@gmail.com>
2023-03-14 06:40:21 +0200

    refactor inheret

--- GifsinMaker/GifMaker.cs
+++ GifsinMaker/GifMaker.cs
@@ -2,21 +2,22 @@
 
 namespace GifsineMaker;
 
-public class GifMaker
+public class GifMaker : GifLoader
 {
-    private List<GifFrame> Gifs { get; set; }
-    public GifMaker(List<GifFrame> gifs)
+    public GifMaker(string inputFile, double maxDelayRatio, double speedUpRatio) : base(inputFile)
     {
-        Gifs = gifs;
+        AddReverse();
+        NewDelays(new GifSineWave(Count, maxDelayRatio * Average)
+        .Absalute(speedUpRatio * Average, (int)Average));
     }
     public void SaveGif(string outputFile)
     {
-        if (!Gifs.Any()) throw new MissingMemberException(nameof(Gifs));
+        if (!Frames.Any()) throw new MissingMemberException(nameof(Frames));
         // Create a new GifCreator object with the adjusted delays
         using (GifCreator creator = AnimatedGif.Create(outputFile, 30))
         {
             // Add the frames to the GifCreator object with the adjusted delays
-            foreach (var item in Gifs)
+            foreach (var item in Frames)
             {
                 creator.AddFrame(item.Frame, item.Delay);
             }
@@ -24,9 +25,9 @@ public class GifMaker
     }
     public GifMaker NewDelays(List<int> Delays)
     {
-        if (Delays.Count != Gifs.Count)
+        if (Delays.Count != Count)
             throw new ArgumentOutOfRangeException(nameof(Delays));
-        Gifs = Gifs.Zip(Delays, (frame, delay) =>
+        Frames = Frames.Zip(Delays, (frame, delay) =>
         {
             frame.Delay = delay;
             return frame;
--- GifsinMaker/Program.cs
+++ GifsinMaker/Program.cs
@@ -9,24 +9,13 @@ class Program
             Console.WriteLine("Usage: program.exe inputfile outputfile maxdelayratio speedupratio");
             return;
         }
-
         if (!double.TryParse(args[2], out double maxDelayRatio) || !double.TryParse(args[3], out double speedUpRatio))
         {
             Console.WriteLine("Error: maxdelayratio and speedupratio must be valid floating-point numbers.");
             return;
         }
-
         string inputFile = args[0];
         string outputFile = args[1];
-
-        Run(inputFile, outputFile, maxDelayRatio, speedUpRatio);
-    }
-
-    private static void Run(string inputFile, string outputFile, double maxDelayRatio,double speedUpRatio)
-    {
-        var frames = new GifLoader(inputFile).AddReverse();
-        var delays = new GifSineWave(frames.Count, maxDelayRatio * frames.Average)
-            .Absalute(speedUpRatio * frames.Average, (int)frames.Average);
-        new GifMaker(frames.Frames).NewDelays(delays).SaveGif(outputFile);
+        new GifMaker(inputFile, maxDelayRatio, speedUpRatio).SaveGif(outputFile);    
     }
 }
