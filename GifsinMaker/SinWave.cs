613f50814751a2bbe929167ee9114e4b181df611
Stan Goldin <48094744+Lomet@users.noreply.github.com>
2023-03-14 02:12:54 +0200

    Update SinWave.cs

--- GifsinMaker/SinWave.cs
+++ GifsinMaker/SinWave.cs
@@ -3,7 +3,7 @@
     public List<int> Delays { get; set; } = new List<int>();
     public int Sum(int min, int value) => Absalute(min, value).Sum();
     public List<int> Absalute(int min,int value) => Delays.Select(delay => Math.Abs(delay) > min ? Math.Abs(delay) : value).ToList();
-    public GifSineWave(int frameCount, double maxDelay = 150, double startAngle =  - 0.5 * Math.PI)
+    public GifSineWave(int frameCount, double maxDelay, double startAngle =  - 0.5 * Math.PI)
     {
         double period = 2 * Math.PI / frameCount;
 
